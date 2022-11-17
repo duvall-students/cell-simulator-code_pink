5a8b392a27ac777c55c344a3c543501873c578bd
amillionkearse <amillionkearse@172.20.10.3>
2022-11-17 14:27:00 -0500

    finished the constructor and adding methods.

--- src/application/RockPaperScissorsController.java
+++ src/application/RockPaperScissorsController.java
@@ -8,11 +8,30 @@ import java.awt.Point;
 
 public class RockPaperScissorsController {
 	
+	private final int NUM_ROWS = 31; 
+	private final int NUM_COLUMNS = 41;
+	
 	//setting up grid
 	private RockPaperScissors rockPaperScissors;
 	private RockPaperScissorsDisplay rockPaperScissorsDisplay;
 	
-	public RockPaperScissorsController() {
-		
+	public RockPaperScissorsController(RockPaperScissorsDisplay view) {
+		int numRows = NUM_ROWS;
+		int numColumns = NUM_COLUMNS;
+		rockPaperScissors = new RockPaperScissors(numRows, numColumns);
+		rockPaperScissorsDisplay = view;
 	}
+	
+	public Point getGridDimensions() {
+		return new Point(NUM_ROWS, NUM_COLUMNS);
+	}
+
+	public int getNumRows() {
+		return NUM_ROWS;
+	}
+	
+	public int getNumCols() {
+		return NUM_COLUMNS;
+	}
+	
 }
